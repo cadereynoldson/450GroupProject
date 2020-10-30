dbac452b29bf08f75af69ead1c36d5dc7fbe5c82
cadereynoldson <cader444@gmail.com>
2020-10-29 18:39:48 -0700

    Navigation between register and sign in now implemented.

--- app/src/main/java/com/example/groupproject_g3/authorization/fragments/RegisterFragment.java
+++ app/src/main/java/com/example/groupproject_g3/authorization/fragments/RegisterFragment.java
@@ -2,13 +2,17 @@ package com.example.groupproject_g3.authorization.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.groupproject_g3.R;
+import com.example.groupproject_g3.databinding.FragmentRegisterBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,11 +20,31 @@ import com.example.groupproject_g3.R;
  */
 public class RegisterFragment extends Fragment {
 
+    private FragmentRegisterBinding binding;
+
+    public RegisterFragment() {
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonRegisterBack.setOnClickListener(button -> {
+            Navigation.findNavController(getView()).navigate(
+                    RegisterFragmentDirections.actionRegisterFragmentToSignInFragment());
+        });
+        binding.buttonRegister.setOnClickListener(this::attemptRegister);
+    }
+
+    private void attemptRegister(final View button) {
+        //TODO: Attempt to register this user.
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
