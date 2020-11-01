b8cfa5383f5710853f569641b779aa1d4c3cc52d
cadereynoldson <cader444@gmail.com>
2020-10-31 23:45:53 -0700

    Bottom navigation implemented. Blank fragments included for navigation.

--- app/src/main/java/com/example/groupproject_g3/activities/MainActivity.java
+++ app/src/main/java/com/example/groupproject_g3/activities/MainActivity.java
@@ -1,22 +1,49 @@
 package com.example.groupproject_g3.activities;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.groupproject_g3.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO: Fetch arguments to this activity and place accordingly.
+        buildMenu();
+    }
 
-        mTextView = (TextView) findViewById(R.id.text);
+    /**
+     * Builds the bottom navigation menu and places it on the main activity display.
+     */
+    private void buildMenu() {
+        BottomNavigationView navView = findViewById(R.id.bottom_nav_menu);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home,
+                R.id.navigation_weather,
+                R.id.navigation_messages,
+                R.id.navigation_contacts,
+                R.id.navigation_profile
+        ).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
