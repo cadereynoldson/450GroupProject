61663323796e5850dc90ffce7517975c882b63c1
Melinda Tran <mtran36@uw.edu>
2020-11-04 22:37:46 -0800

    Implement Sign In

--- app/src/main/java/com/example/groupproject_g3/authorization/fragments/SignInViewModel.java
+++ app/src/main/java/com/example/groupproject_g3/authorization/fragments/SignInViewModel.java
@@ -4,9 +4,23 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+
+import org.json.JSONObject;
 
 public class SignInViewModel extends AndroidViewModel {
+
     public SignInViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer) {
+        //TODO: something
+    }
+
+    public void connect(final String email, final String password) {
+        //TODO: something
     }
 }
