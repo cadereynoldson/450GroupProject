18514e75351ad60098dc8871c9dcfe626ffb9d2d
Melinda Tran <mtran36@uw.edu>
2020-11-04 22:19:03 -0800

    Implement Register and SignIn

---
+++ app/src/main/java/com/example/groupproject_g3/authorization/fragments/RegisterViewModel.java
@@ -0,0 +1,16 @@
+package com.example.groupproject_g3.authorization.fragments;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class RegisterViewModel extends AndroidViewModel {
+
+    public RegisterViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void connect(String toString, String toString1, String toString2, String toString3) {
+    }
+}
---
+++ app/src/main/java/com/example/groupproject_g3/authorization/fragments/SignInViewModel.java
@@ -0,0 +1,12 @@
+package com.example.groupproject_g3.authorization.fragments;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class SignInViewModel extends AndroidViewModel {
+    public SignInViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
