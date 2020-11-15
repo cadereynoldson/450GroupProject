a85b601bee2106bb3d7d6acdc3c87162412cded5
cadereynoldson <cader444@gmail.com>
2020-11-14 19:33:03 -0800

    Update register page and began contacts page.

---
+++ app/src/main/java/com/example/groupproject_g3/contact/fragments/ContactRecyclerViewAdapter.java
@@ -0,0 +1,32 @@
+package com.example.groupproject_g3.contact.fragments;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ContactRecyclerViewAdapter extends RecyclerView.Adapter<ContactRecyclerViewAdapter.ContactViewHolder> {
+    @NonNull
+    @Override
+    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
