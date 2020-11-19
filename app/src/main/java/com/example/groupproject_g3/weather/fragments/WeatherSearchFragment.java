969b3fffc49f3441721b4b7a0379716037695a78
mtran36 <tranmelinda84@yahoo.com>
2020-11-18 22:11:53 -0800

    Weather Modified

--- app/src/main/java/com/example/groupproject_g3/weather/fragments/WeatherSearchFragment.java
+++ app/src/main/java/com/example/groupproject_g3/weather/fragments/WeatherSearchFragment.java
@@ -2,23 +2,37 @@ package com.example.groupproject_g3.weather.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.groupproject_g3.R;
+import com.example.groupproject_g3.databinding.FragmentPageWeatherSearchBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WeatherSearchFragment extends Fragment {
 
+    private FragmentPageWeatherSearchBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_search, container, false);
+        return inflater.inflate(R.layout.fragment_page_weather, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.searchWeather.setOnClickListener(button ->
+                Navigation.findNavController(getView()).navigate(
+                        WeatherSearchFragmentDirections.actionNavigationWeatherSearchToNavigationWeather()));
     }
 }
--- app/src/main/java/com/example/groupproject_g3/weather/fragments/WeatherViewModel.java
+++ app/src/main/java/com/example/groupproject_g3/weather/fragments/WeatherViewModel.java
@@ -11,6 +11,11 @@ import com.example.groupproject_g3.contact.fragments.ContactItem;
 import java.util.List;
 
 public class WeatherViewModel extends AndroidViewModel {
+
+    private MutableLiveData<List<ContactItem>> mWeather;
+
+    private static final String connectionUrl = "https://cloud-chat-450.herokuapp.com/weathers/";
+
     public WeatherViewModel(@NonNull Application application) {
         super(application);
     }
